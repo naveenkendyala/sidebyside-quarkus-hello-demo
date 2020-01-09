7dd684036a98c5b6633417c1e3583219662c2905
Naveen Kendyala <naveenkendyala@kendyala.com>
2020-01-09 13:52:17 -0500

    Updated max limit on pods

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -1,4 +1,4 @@
 oc apply -f deployment.yaml
 oc expose deployment.apps/quarkus-hello-deploy
 oc expose service/quarkus-hello-deploy
-oc autoscale deploy/quarkus-hello-deploy --min 1 --max 5 --cpu-percent=50
+oc autoscale deploy/quarkus-hello-deploy --min 1 --max 10 --cpu-percent=50
